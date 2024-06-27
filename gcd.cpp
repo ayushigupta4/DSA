08bd97ac5a648936dccdf755b2f08e65a4920557
Ayushi Gupta <ayushi.gupta.038@gmail.com>
2024-06-27 15:20:27 +0530

    GCD without recursion

---
+++ gcd.cpp
@@ -0,0 +1,34 @@
+//not working
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if(a==0) {
+        return b;
+    }
+
+    if(b==0) {
+        return a;
+    }
+
+    while(a!=b) {
+        if(a>b) {
+            a = a-b;
+        }
+        else {
+            b = b-a;
+        }
+    }
+
+    return a;
+}
+
+int main() {
+    int a,b;
+    cout << "Enter 2 values: ";
+    cin >> a;
+    cin >> b;
+    int ans = gcd(a,b);
+    cout << "GCD: " << ans;
+    return 0;
+}
